b22d1b7183eee89ab662e7a8649e92c2d3f6dcda
Kwabena Anane Kwarteng <kwabena.ananeK@gmail.com>
2025-06-15 00:23:06 +0200

    Add security like helmet, cors, sanitize in app.mjs

--- BackEnd/src/app.mjs
+++ BackEnd/src/app.mjs
@@ -1,17 +1,43 @@
 import express from 'express';
-import dotenv from 'dotenv';
 import cors from 'cors';
-
+import rateLimit from 'express-rate-limit';
+// import mongoSanitize from 'express-mongo-sanitize';
+import { xss } from 'express-xss-sanitizer';
+import helmet from 'helmet';
+import hpp from 'hpp';
+import dotenv from 'dotenv';
 import { connectDb } from './db/database.mjs';
 
 dotenv.config({ path: './config/config.env' });
 
-await connectDb()
+await connectDb();
+
+// Konfigurera regler för begränsning av förfrågningar ifrån en och samma ip-adress
+const limiter = rateLimit({
+  max: 100, 
+  windowMs: 60 * 60 * 1000,
+  message: 'You are killing me, get lost!!!',
+});
 
 const app = express();
 
+app.use(helmet());
+
 app.use(cors());
 
-app.use(express.json());
+app.use('/api/', limiter);
+
+app.use(express.json({ limit: '10kb' }));
+
+
+// app.use(
+//   mongoSanitize({
+//     replaceWith: '_',
+//   })
+// );
+
+app.use(xss());
+
+app.use(hpp());
 
 export { app };
