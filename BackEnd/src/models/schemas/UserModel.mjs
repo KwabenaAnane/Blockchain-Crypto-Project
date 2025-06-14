165f607655ae296ae4b4f4695c00e31fe68918fd
Kwabena Anane Kwarteng <kwabena.ananeK@gmail.com>
2025-06-15 00:53:07 +0200

    Fix UserModel.mjs

---
+++ BackEnd/src/models/schemas/UserModel.mjs
@@ -0,0 +1,48 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+import validator from 'validator';
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: [true, 'First name is required'],
+  },
+  lastName: {
+    type: String,
+    required: [true, 'Last name is required'],
+  },
+  email: {
+    type: String,
+    required: [true, 'E-mail is required'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin', 'miner', 'sales'],
+    default: 'user',
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    minlength: 8,
+    select: false,
+  },
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+userSchema.methods.checkPassword = async function (
+  passwordToCheck,
+  userPassword
+) {
+  return await bcrypt.compare(passwordToCheck, userPassword);
+};
+
+export default mongoose.model('User', userSchema);
